304e216e0496a97fb44427bb8132c9edf69ecc2b
dualism98 <beachsidebearhi@gmail.com>
2021-06-01 19:52:24 +0700

    Added all necessary components, created HomeScreen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# devoops
+# DevOops
 
 ## Project setup
 ```
@@ -19,6 +19,3 @@ npm run build
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h2 class='home-text'>Добро пожаловать в</h2>
+    <h2 class='devoops'>DevOops</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style scoped>
+  .home-text {
+    color: white;
+    margin: 0;
+    margin-top: 20vh;
+  }
+
+  .devoops {
+    color: white;
+    margin: 0;
+  }
+
+  @media screen and (min-width: 601px) {
+    h2.home-text {
+      font-size: 50px;
+    }
+    h2.devoops {
+      font-size: 100px;
+    }
+  }
+
+/* If the screen size is 600px wide or less, set the font-size of <div> to 30px */
+  @media screen and (max-width: 600px) {
+    h2.home-text {
+      font-size: 30px;
+    }
+    h2.devoops {
+      font-size: 50px;
+    }
+  }
+</style>
