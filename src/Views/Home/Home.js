3c4429b951824f2335ce3c5073a7238e2ee273c2
w0rng <antonabramov2000@gmail.com>
2021-06-03 12:03:24 +0700

    Поменял иконку и название сайта

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 export default function HomeScreen(){
     return(
         <div class="home">
-            <h2 class='home-text'>Добро пожаловать в</h2>
+            <h1 class='home-text'>Добро пожаловать в</h1>
             <h2 class='devoops'>Dev😯ops</h2>
         </div>
     )
